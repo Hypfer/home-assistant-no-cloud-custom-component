3ba2f861d97f3a0c45f8539dcb7ec6ca2a366e29
Hypfer <soeren@soerenbeye.de>
2020-10-11 12:15:32 +0200

    Mock more things to get an error-free home assistant log

--- custom_components/cloud/__init__.py
+++ custom_components/cloud/__init__.py
@@ -1,7 +1,44 @@
+import voluptuous as vol
+from homeassistant.exceptions import HomeAssistantError
+from homeassistant.core import callback
+from homeassistant.loader import bind_hass
+from homeassistant.components import websocket_api
+
+
 DOMAIN = "cloud"
 
 
+WS_TYPE_STATUS = "cloud/status"
+SCHEMA_WS_STATUS = websocket_api.BASE_COMMAND_MESSAGE_SCHEMA.extend(
+    {vol.Required("type"): WS_TYPE_STATUS}
+)
+
+
 async def async_setup(hass, config):
+    async_register_command = hass.components.websocket_api.async_register_command
+    async_register_command(WS_TYPE_STATUS, websocket_cloud_status, SCHEMA_WS_STATUS)
 
     # Return boolean to indicate that initialization was successful.
     return True
+
+
+class CloudNotAvailable(HomeAssistantError):
+    """Raised when an action requires the cloud but it's not available."""
+
+
+@bind_hass
+@callback
+def async_remote_ui_url(hass) -> str:
+    """Get the remote UI URL."""
+    raise CloudNotAvailable
+
+
+@callback
+def websocket_cloud_status(hass, connection, msg):
+    """Handle request for account info.
+    Async friendly.
+    """
+
+    connection.send_message(
+        websocket_api.result_message(msg["id"], {"logged_in": False, "cloud": "disconnected"})
+    )
