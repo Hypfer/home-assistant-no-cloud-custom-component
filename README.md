302f665fe76476478f84cd1c6abd2aab10c2f18b
Hypfer <soeren@soerenbeye.de>
2020-10-12 16:43:58 +0200

    Override get_config ws command to remove "cloud" from loaded components

--- README.md
+++ README.md
@@ -11,4 +11,11 @@ You should be able to add this as a custom repository URL to HACS.
 This will however not be added to the default HACS repository since it makes sense for regular users to not override the cloud and updater components.
 If you really want to do this, you should be "advanced" enough to add a custom repository.
 
+
+Do note that this also modifies the `get_config` handler of the core webhook integration to remove the `cloud` component from the list
+of active integrations, because the frontend enables stuff such as the almond automation dialog when it detects that.
+
+This _could_ be destructive and break other things. Please don't complain about that in the Home Assistant Issue tracker.
+Instead, complain about the fact that there's no way to customize `default_config`.
+
 Also, feel free to check out [the updater: counterpart](https://github.com/Hypfer/home-assistant-no-updater-custom-component)
--- custom_components/cloud/__init__.py
+++ custom_components/cloud/__init__.py
@@ -13,13 +13,30 @@ SCHEMA_WS_STATUS = websocket_api.BASE_COMMAND_MESSAGE_SCHEMA.extend(
     {vol.Required("type"): WS_TYPE_STATUS}
 )
 
+WS_TYPE_GET_CONFIG = "get_config"
+SCHEMA_WS_GET_CONFIG = websocket_api.BASE_COMMAND_MESSAGE_SCHEMA.extend(
+    {vol.Required("type"): WS_TYPE_GET_CONFIG}
+)
+
 
 async def async_setup(hass, config):
     async_register_command = hass.components.websocket_api.async_register_command
     async_register_command(WS_TYPE_STATUS, websocket_cloud_status, SCHEMA_WS_STATUS)
 
+    async_register_command(WS_TYPE_GET_CONFIG, handle_get_config, SCHEMA_WS_GET_CONFIG)
+
+    hass.async_create_task(reregister_get_config(hass))
     # Return boolean to indicate that initialization was successful.
     return True
+
+
+async def reregister_get_config(hass):
+    """Since there apparently can be a race-condition here, we're just trying to re-register later
+        Yes, this is terrible but what can you do?
+    """
+    async_register_command = hass.components.websocket_api.async_register_command
+
+    async_register_command(WS_TYPE_GET_CONFIG, handle_get_config, SCHEMA_WS_GET_CONFIG)
 
 
 class CloudNotAvailable(HomeAssistantError):
@@ -42,3 +59,14 @@ def websocket_cloud_status(hass, connection, msg):
     connection.send_message(
         websocket_api.result_message(msg["id"], {"logged_in": False, "cloud": "disconnected"})
     )
+
+
+@callback
+def handle_get_config(hass, connection, msg):
+    """Handle get config command."""
+
+    current_config = hass.config.as_dict()
+    if DOMAIN in current_config['components']:
+        current_config['components'].remove(DOMAIN)
+
+    connection.send_message(websocket_api.result_message(msg["id"], current_config))
